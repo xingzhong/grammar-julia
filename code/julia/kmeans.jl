674d0f636825bc4ca9f87a68c46bd568ab15f8ee
Xingzhong <xu.xeno@gmail.com>
2013-05-12 10:59:48 -0400

    add cluster

---
+++ code/julia/kmeans.jl
@@ -0,0 +1,21 @@
+using Clustering
+using Winston
+
+include("loader.jl")
+fileName = "../../sample/P3_2_8_p02.csv"
+#fileName = "../../sample/P1_1_12_p19.csv"
+println("\nloading data "*fileName)
+frame = DataMS(fileName)
+test = transpose(frame.ob[:, 1:2, 7]) # 119
+#test = randn(2, 500)
+res = kmeans(test, 4)
+show(Points(res.centers[1,:], res.centers[2,:]))
+p = FramedPlot()
+data = Points(test[1,:], test[2,:], "type", "circle", "symbolsize", "0.5")
+setattr(data, "label", "data")
+label = Points(res.centers[1,:], res.centers[2,:], "type", "filled circle", "color", "red")
+setattr(data, "label", "label")
+
+add(p, data)
+add(p, label)
+file(p, "kmean.png")
--- code/julia/test.jl
+++ code/julia/test.jl
@@ -1,3 +1,37 @@
-for i in 1:1000
-	@printf("\r%f", i/100)
-end
+include("Grammar.jl")
+include("Inside.jl")
+
+TEST = [ ones(14, 1) zeros(14, 1) ]
+TEST = TEST + 0.4* randn(14,2)
+x = linspace( 0, 3*pi, 15 )
+TEST2 = [diff(x) diff(sin(x))]
+TEST2 = TEST2 + 0.4* randn(14,2)
+x1 = linspace( -5, 5, 8 )
+x2 = linspace(5,-5, 8)
+TEST3 = [diff(x1) diff( sqrt(36-x1.^2))]
+TEST3 = [TEST3; diff(x2) diff( -sqrt(36-x2.^2))]
+TEST3 = TEST3 + 0.4* randn(14,2)
+
+#TEST = TEST2
+TEST = TEST3
+show(TEST)
+grammar = "./line.cnf"
+
+A, B, S = loadGrammar(grammar)
+println("CYK parsing"*grammar)
+GT = CYK(TEST, A, B)
+println("Parsing Tree")
+loglik = buildTree(GT, S, TEST)
+println(loglik)
+println()
+
+using Winston
+path = cumsum(TEST)
+p = FramedPlot("title", string(loglik))
+setattr(p, "xrange", (-15,15))
+setattr(p, "yrange", (-10,10))
+setattr(p, "aspect_ratio", 1)
+setattr(p.frame1, "draw_grid", true )
+add(p, Points(path[:,1], path[:,2], "symboltype", "half filled triangle"))
+add(p, Curve(path[:,1], path[:,2], "linetype", "longdashed"))
+file(p, "test.png")
